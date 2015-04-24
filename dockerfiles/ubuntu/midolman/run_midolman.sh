4338bd7851b6a83733927aa5ea9ebf2a02725a46
Ivan Kelly <ivan@midokura.com>
2015-04-24 10:35:25 +0000

    1.8.4 for mn2950

--- dockerfiles/ubuntu/midolman/run_midolman.sh
+++ dockerfiles/ubuntu/midolman/run_midolman.sh
@@ -1,6 +1,26 @@
 #!/bin/bash
 
-sed -i '/^servers/c\servers = '$CS_PORT_9042_TCP_ADDR:$CS_PORT_9042_TCP_PORT'' /etc/midolman/midolman.conf
-sed -i '/^zookeeper_hosts/c\zookeeper_hosts = '$ZK_PORT_2181_TCP_ADDR:$ZK_PORT_2181_TCP_PORT'' /etc/midolman/midolman.conf 
+echo "Running midolman"
+
+if [ ! -e /etc/nsuuid ]; then
+    uuidgen | cut -b 1-8 > /etc/nsuuid
+fi
+
+UUID=$(cat /etc/nsuuid)
+
+NS=ns-$UUID
+echo $NS
+
+ip netns add $NS
+for i in $(seq 1 20); do
+    DP=dp-$UUID-$i
+    PR=pr-$UUID-$i
+    ip link add name $DP type veth peer name $PR
+    ip link set $DP up
+    ip link set $PR netns $NS
+    ip netns exec $NS ip link set $PR up
+    ip netns exec $NS ip address add 172.16.30.1/24 dev $PR
+    ip netns exec $NS ifconfig lo up
+done
 
 exec /usr/share/midolman/midolman-start
